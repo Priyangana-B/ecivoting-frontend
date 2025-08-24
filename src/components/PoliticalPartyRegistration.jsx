5c5ba5dfb777b6cc1eb6ac8c9741be799a2b9eb3
Priyangana-B <priyanganab07@gmail.com>
2025-08-24 10:53:50 +0530

    image final

--- src/components/PoliticalPartyRegistration.jsx
+++ src/components/PoliticalPartyRegistration.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import img3 from "../assets/Images/image3.jpg";
 import "../assets/css/PoliticalPartiesCandidates/PoliticalPartyRegistration.css"; // Updated path
 
 const PoliticalPartyRegistration = () => {
@@ -79,7 +78,6 @@ const PoliticalPartyRegistration = () => {
       {/* Hero Section */}
       <div className="page-header">
         <div className="container">
-          <img src={img3} alt="Political Party Registration" />
           <h1>Political Parties Registration</h1>
           <p>Registration and management of political parties under electoral laws</p>
         </div>
